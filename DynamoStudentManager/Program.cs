39a9b17f2b1d4c52e79f758d54322d3098d17fc4
Anand Shende <anandshende1994@gmail.com>
2023-03-29 20:25:46 +0530

    removing https

--- DynamoStudentManager/Program.cs
+++ DynamoStudentManager/Program.cs
@@ -17,13 +17,8 @@ builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
